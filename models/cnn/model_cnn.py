859bc37a660fd00df5a4b7e142ee3a2bb7f2aca5
kurowasan <philippebrouillard@gmail.com>
2018-01-11 15:48:45 -0500

    Removed comments

--- models/cnn/model_cnn.py
+++ models/cnn/model_cnn.py
@@ -16,33 +16,14 @@ class CNN(nn.Module):
         self.conv1 = nn.ModuleList([nn.Conv2d(1, n_kernel, (k, d)) for k in
                                     kernels_dim])
 
-        # self.conv1 = nn.Conv2d(1, 1, kernel_size=(7, d), stride=1)
-        # self.conv2 = nn.Conv2d(1, 5, (7, d))
-        # self.conv3 = nn.Conv2d(1, 10, (3, d))
-        # self.conv4 = nn.Conv2d(1, 1, (3, d))
-        # self.conv5 = nn.Conv2d(1, 1, (3, d))
-        # self.conv6 = nn.Conv2d(1, 1, (3, d))
-
         self.dropout = nn.Dropout(0.25)
         self.linear1 = nn.Linear(self.n_kernel * len(self.kernels_dim), self.n_output1)
 
-    def conv_and_pool(self, x):
-        x = F.relu(x).squeeze(-1)
-        x = F.max_pool1d(x, x.size(2)).squeeze(2)
-        return x
-
     def forward(self, x):
-        # import pdb; pdb.set_trace()
         x = x.unsqueeze(1)
-        # x = self.conv_and_pool(self.conv1(x))
-        # x = self.conv_and_pool(self.conv2(x))
-        # x = self.conv_and_pool(self.conv3(x))
-        # x = self.conv_and_pool(self.conv4(x))
-        # x = self.conv_and_pool(self.conv5(x))
-        # x = self.conv_and_pool(self.conv6(x))
-
         x = [F.relu(conv(x)).squeeze(3) for conv in self.conv1] #[(N,Co,W), ...]*len(Ks)
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x] #[(N,Co), ...]*len(Ks)
+        import pdb; pdb.set_trace()
         x = torch.cat(x, 1)
 
         x = self.dropout(x) # (N,len(Ks)*Co)
